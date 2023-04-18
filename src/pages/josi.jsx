1da050b6738e41f6d2068808b1ec4463babd048a
josi43 <mateusseabra@hotmail.com>
2023-04-17 21:57:06 -0300

    meu commit 200

--- src/pages/josi.jsx
+++ src/pages/josi.jsx
@@ -2,7 +2,7 @@ export default function Josi() {
     return (
         <main>
             <h1>Josi</h1>
-            <a>teste</a>
+            <a>essa e a pagina do seabra! Rala daqui mlk!</a>
         </main>
     )
 }
